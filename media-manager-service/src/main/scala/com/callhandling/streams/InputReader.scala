41f0a251697d4c1158fc372dca7f232c604b0b2f
Melvic <melvic.ybanez2014@gmail.com>
2019-06-10 14:22:51 +0800

    Refactor stream package

---
+++ media-manager-service/src/main/scala/com/callhandling/streams/InputReader.scala
@@ -0,0 +1,10 @@
+package com.callhandling.streams
+
+trait InputReader[R] {
+  def read(reader: R, id: String): InputStream
+}
+
+object InputReader {
+  def read[R](reader: R, id: String)(implicit inputReader: InputReader[R]): InputStream =
+    inputReader.read(reader, id)
+}
