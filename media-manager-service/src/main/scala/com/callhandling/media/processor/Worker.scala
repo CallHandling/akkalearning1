e7b49a7dd914ae60d88a8a74f2ff59fe41e4e313
Melvic <melvic.ybanez2014@gmail.com>
2019-06-10 20:37:23 +0800

    Incorporate worker with audio processor

--- media-manager-service/src/main/scala/com/callhandling/media/processor/Worker.scala
+++ media-manager-service/src/main/scala/com/callhandling/media/processor/Worker.scala
@@ -1,13 +1,29 @@
 package com.callhandling.media.processor
 
-import akka.actor.Actor
-import com.callhandling.media.streams.{InputReader, OutputWriter}
+import akka.actor.{Actor, Props}
+import com.callhandling.media.OutputFormat
+import com.callhandling.media.processor.Worker.Convert
+import com.callhandling.media.streams.{InputReader, InputStream, OutputWriter}
 
 object Worker {
+  def props[O](
+      id: String,
+      inputStream: InputStream,
+      output: O,
+      outputFormat: OutputFormat)
+      (implicit writer: OutputWriter[O]): Props =
+    Props(new Worker[O](id, inputStream, output, outputFormat))
 
+  case object Convert
 }
 
-class Worker[R, W](reader: R, writer: W)
-    (implicit inputStream: InputReader[R], outputWriter: OutputWriter[W]) extends Actor {
-  override def receive = ???
+class Worker[O](
+    id: String,
+    inputStream: InputStream,
+    output: O,
+    outputFormat: OutputFormat)
+    (implicit writer: OutputWriter[O]) extends Actor {
+  override def receive = {
+    case Convert =>
+  }
 }
