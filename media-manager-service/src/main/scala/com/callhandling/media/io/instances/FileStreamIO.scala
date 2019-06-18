f57f09680089349e5fe289303336d534321dfc2e
Melvic <melvic.ybanez2014@gmail.com>
2019-06-18 18:55:49 +0800

    Optimize imports and refactor type parameters

--- media-manager-service/src/main/scala/com/callhandling/media/io/instances/FileStreamIO.scala
+++ media-manager-service/src/main/scala/com/callhandling/media/io/instances/FileStreamIO.scala
@@ -1,12 +1,11 @@
 package com.callhandling.media.io.instances
 
-import java.io.File
 import java.nio.file.{Path, Paths}
 
 import akka.stream.IOResult
 import akka.stream.scaladsl.{FileIO, Sink, Source}
 import akka.util.ByteString
-import com.callhandling.media.{OutputFormat, StreamDetails}
+import com.callhandling.media.StreamDetails
 import com.callhandling.util.FileUtil._
 
 import scala.concurrent.Future
