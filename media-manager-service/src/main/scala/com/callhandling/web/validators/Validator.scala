622907f342ff07fe9efaa5192977030bae3f1b4b
Melvic <melvic.ybanez2014@gmail.com>
2019-06-25 17:31:41 +0800

    Implement required validation

--- media-manager-service/src/main/scala/com/callhandling/web/validators/Validator.scala
+++ media-manager-service/src/main/scala/com/callhandling/web/validators/Validator.scala
@@ -3,17 +3,22 @@ package com.callhandling.web.validators
 import akka.http.scaladsl.server.Directives.{provide, reject}
 import akka.http.scaladsl.server.Route
 
-trait Validator[T] extends (T => Seq[FieldErrorInfo])
+trait Validator[F] extends (F => Seq[FieldErrorInfo])
 
 object Validator {
-  def validateForm[T](form: T)(f: T => Route)(implicit validator: Validator[T]): Route = {
+  def validateForm[F](form: F)(f: F => Route)(implicit validator: Validator[F]): Route = {
     validator(form) match {
-      case Nil => provide(form)(f)
-      case errors: Seq[FieldErrorInfo] => reject(FormValidationRejection(errors))
+      case Vector.empty => provide(form)(f)
+      case errors: Vector[FieldErrorInfo] => reject(FormValidationRejection(errors))
     }
   }
 
-  def validation(validation: Validation[Any], fieldName: String, field: Any): Option[FieldErrorInfo] =
+  /*
+  def validateField[V](validation: FieldValidation[V], fieldName: String, field: Any): Option[FieldErrorInfo] =
     if(validation.rule(field)) Some(FieldErrorInfo(fieldName, validation.errorMessage(fieldName)))
+    else None*/
+
+  def validate[F](fieldName: String, field: F)(implicit validation: FieldValidation[F]) =
+    if (validation.validate(field)) Some(FieldErrorInfo(fieldName, validation.errorMessage(fieldName)))
     else None
 }
--- media-manager-service/src/main/scala/com/callhandling/web/validators/package.scala
+++ media-manager-service/src/main/scala/com/callhandling/web/validators/package.scala
@@ -1,32 +1,40 @@
 package com.callhandling.web
 
-import Forms.{ConversionStatusForm, ConvertFileForm, FileIdForm, FormatForm, PlayForm, UploadFileForm}
+import com.callhandling.web.Forms._
+import com.callhandling.web.validators.Required._
 import com.callhandling.web.validators.Validator._
-import com.callhandling.web.validators.ValidationUtils._
 
-// TODO: Redundant validations on multiple validators (code duplications)
 package object validators {
+  type Rule[A] = A => Boolean
+
   implicit val uploadFileFormValidator: Validator[UploadFileForm] = model =>
-    validation(minValidation(5), "description", model.description).toVector
+    validate(minValidation(5), "description", model.description).toVector
 
   implicit val convertFileFormValidator: Validator[ConvertFileForm] = {
-    case ConvertFileForm(fileId, format, channels, sampleRate, codec) => withRequiredId(fileId,
-      validation(requiredValidation, "format", format),
-      validation(requiredValidation, "channels", channels),
-      validation(requiredValidation, "sampleRate", sampleRate),
-      validation(requiredValidation, "codec", codec))
+    case ConvertFileForm(fileId, format, channels, sampleRate, codec) =>
+      errorMessages(
+      requiredId(fileId),
+      validate("format", format)(Required[String]),
+      validate("channels", channels)(Required[Int]),
+      validate("sampleRate", sampleRate)(Required[Int]),
+      validate("codec", codec)(Required[String]))
   }
 
-  implicit val fileIdFormValidator: Validator[FileIdForm] = model => withRequiredId(model.fileId)
+  implicit val fileIdFormValidator: Validator[FileIdForm] = model =>
+    errorMessages(requiredId(model.fileId))
 
   // TODO: check against the supported output formats for the file
   implicit val conversionStatusFormValidator: Validator[ConversionStatusForm] = {
-    case ConversionStatusForm(fileId, format) => Vector(
-      validation(requiredValidation, "fileId", fileId),
-      validation(requiredValidation, "format", format)).flatten
+    case ConversionStatusForm(fileId, format) => errorMessages(
+      requiredId(fileId),
+      validate("format", format))
   }
 
-  implicit val playFormValiator: Validator[PlayForm] = { case PlayForm(fileId, format) =>
-    Vector(validation(requiredValidation, "fileId", fileId)).flatten
+  implicit val playFormValidator: Validator[PlayForm] = { case PlayForm(fileId, format) =>
+    errorMessages(requiredId(fileId))
   }
+
+  def requiredId(value: String) = validate("fileId", value)(Required[String])
+
+  def errorMessages(results: Option[FieldErrorInfo]*) = results.toVector.flatten
 }
