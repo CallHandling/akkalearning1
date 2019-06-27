2b36072ea332cc434c2c5c0b4ec25b635a5f7d7a
Melvic <melvic.ybanez2014@gmail.com>
2019-06-27 13:23:23 +0800

    Add comments for the io types

--- media-manager-service/src/main/scala/com/callhandling/media/io/package.scala
+++ media-manager-service/src/main/scala/com/callhandling/media/io/package.scala
@@ -8,12 +8,17 @@ import akka.util.ByteString
 import scala.concurrent.Future
 
 package object io {
+  // Base type for stream IO results
   type MediaIO[IO[_, _], M] = IO[ByteString, Future[M]]
 
+  // Specific types of IO results
   type BytesInlet[M] = MediaIO[Source, M]
   type BytesOutlet[M] = MediaIO[Sink, M]
 
-  type IOOr[B] = Either[IOError, B]
+  // Base type for IO results with error handling capability
+  type IOOr[A] = Either[IOError, A]
+
+  // Specific types of IO results that may fail
   type InletOr[M] = IOOr[BytesInlet[M]]
   type OutletOr[M] = IOOr[BytesOutlet[M]]
 }
